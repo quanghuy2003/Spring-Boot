5fa19d69d64f7392ee9ba1e1c04ac0ec22523e15
quanghuy2002 <hoangqunghuy753@gmail.com>
2021-12-24 11:20:33 +0700

    bài th3

---
+++ src/main/java/com/example/customermanagement/service/ICustomerService.java
@@ -0,0 +1,8 @@
+package com.example.customermanagement.service;
+
+import com.example.customermanagement.model.Customer;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ICustomerService extends IGeneralService<Customer> {
+}
---
+++ src/main/java/com/example/customermanagement/service/IGeneralService.java
@@ -0,0 +1,16 @@
+package com.example.customermanagement.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public interface IGeneralService <T>{
+    Iterable<T> findAll();
+
+    Optional<T> findById(Long id);
+
+    void save(T t);
+
+    void remove(Long id);
+}
